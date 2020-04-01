0bfe57aeb5bb1a7d26b57104df70b6ce633fda80
Melnikov, Stanislav <smelnikov@definiens.com>
2020-04-01 19:51:58 +0200

    add functions to get image size

--- tests/test_jxrcodec.cpp
+++ tests/test_jxrcodec.cpp
@@ -16,15 +16,20 @@ static std::string getTestImagePath(const std::string& imageName)
 }
 
 
-void testJxrFileDecoder(const std::string& jxrImageName, const std::string& rawImageName, int imageWidth, int imageHeight, int imageChannels)
+void testJxrFileDecoder(const std::string& jxrImageName, const std::string& rawImageName)
 {
-    const int imageSize = imageWidth*imageHeight*imageChannels;
-    std::vector<uint8_t> raster(imageSize);
+
     std::string imagePath = getTestImagePath(jxrImageName);
     std::string rawImagePath = getTestImagePath(rawImageName);
 
     FILE* fd = fopen(imagePath.c_str(), "rb");
     ASSERT_TRUE(fd!=nullptr);
+
+    uint32_t imageWidth(0), imageHeight(0), imageChannels(3);
+    jpegxr_get_image_size(fd, imageWidth, imageHeight);
+    const int imageSize = imageWidth*imageHeight*imageChannels;
+    std::vector<uint8_t> raster(imageSize);
+
     jpegxr_decompress(fd, raster.data(), (uint32_t)raster.size());
     fclose(fd);
 
@@ -37,23 +42,27 @@ void testJxrFileDecoder(const std::string& jxrImageName, const std::string& rawI
 
 TEST(jxrcodec, decodeFileWdp)
 {
-    testJxrFileDecoder("seagull.wdp", "seagull.raw", 1000, 667, 3);
+    testJxrFileDecoder("seagull.wdp", "seagull.raw");
 }
 
 TEST(jxrcodec, decodeFileJxr)
 {
-    testJxrFileDecoder("sample.jxr", "sample.raw", 128, 128, 3);
+    testJxrFileDecoder("sample.jxr", "sample.raw");
 }
 
-void testJxrMemDecoder(const std::string& jxrImageName, const std::string& rawImageName, int imageWidth, int imageHeight, int imageChannels)
+void testJxrMemDecoder(const std::string& jxrImageName, const std::string& rawImageName)
 {
-    const int imageSize = imageWidth*imageHeight*imageChannels;
-    std::vector<uint8_t> raster(imageSize);
     std::string imagePath = getTestImagePath(jxrImageName);
     std::string rawImagePath = getTestImagePath(rawImageName);
 
     std::ifstream srcFile(imagePath.c_str(), std::ios::binary);
     std::vector<unsigned char> src((std::istreambuf_iterator<char>(srcFile)), std::istreambuf_iterator<char>());
+    uint32_t imageWidth(0), imageHeight(0), imageChannels(3);
+
+    jpegxr_get_image_size(src.data(), src.size(), imageWidth, imageHeight);
+
+    const int imageSize = imageWidth*imageHeight*imageChannels;
+    std::vector<uint8_t> raster(imageSize);
 
     jpegxr_decompress(src.data(), (uint32_t)src.size(), raster.data(), (uint32_t)raster.size());
 
@@ -83,20 +92,42 @@ void createRaw(const std::string& jxrImageName, const std::string& rawImageName,
 
 TEST(jxrcodec, decodeBufferWdp)
 {
-    testJxrMemDecoder("seagull.wdp", "seagull.raw", 1000, 667, 3);
+    testJxrMemDecoder("seagull.wdp", "seagull.raw");
 }
 
 TEST(jxrcodec, decodeBufferJxr)
 {
-    testJxrMemDecoder("sample.jxr", "sample.raw", 128, 128, 3);
+    testJxrMemDecoder("sample.jxr", "sample.raw");
 }
 
 TEST(jxrcodec, decodeBufferJxr2)
 {
-    testJxrMemDecoder("test.jxr", "test.raw", 1920, 1080, 3);
+    testJxrMemDecoder("test.jxr", "test.raw");
 }
 
 TEST(jxrcodec, decodeBufferJxrTissue)
 {
-    testJxrMemDecoder("tissue.jxr", "tissue.raw", 550, 345, 3);
+    testJxrMemDecoder("tissue.jxr", "tissue.raw");
+}
+
+TEST(jxrcodec, imageSizeMem)
+{
+    std::string imagePath = getTestImagePath("tissue.jxr");
+    std::ifstream srcFile(imagePath.c_str(), std::ios::binary);
+    std::vector<unsigned char> src((std::istreambuf_iterator<char>(srcFile)), std::istreambuf_iterator<char>());
+    uint32_t width(0), height(0);
+    jpegxr_get_image_size(src.data(), (uint32_t)src.size(), width, height);
+    EXPECT_EQ(width, 550);
+    EXPECT_EQ(height, 345);
+}
+
+TEST(jxrcodec, imageSizeFile)
+{
+    std::string imagePath = getTestImagePath("tissue.jxr");
+    FILE* file = fopen(imagePath.c_str(), "rb");
+    uint32_t width(0), height(0);
+    jpegxr_get_image_size(file, width, height);
+    fclose(file);
+    EXPECT_EQ(width, 550);
+    EXPECT_EQ(height, 345);
 }
